a43f70dc3a5e1827c365525fe8b3fb85c59d9fe3
shwejan raj <shwejanraj297@gmail.com>
2024-07-25 20:53:05 +0530

    to check new git setup in mac

--- multiply.java
+++ multiply.java
@@ -3,5 +3,11 @@ public class multiply {
         int a = 4;
         int b = 3;
         System.out.println("The product is : "+(a*b));
+
+        System.out.println("multiply a and b "+(multiply(4,5)));
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
     }
 }
